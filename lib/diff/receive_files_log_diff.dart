ecd3ae999d46c8104dcd4fe4ca15fefe83e64121
digitalevers <opensource@digitalevers.com>
2024-01-19 13:04:48 +0800

    fix

--- lib/diff/receive_files_log_diff.dart
+++ lib/diff/receive_files_log_diff.dart
@@ -1,7 +1,9 @@
 //各平台界面差异化
 import 'dart:io';
 
+import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:path/path.dart' as p;
 import 'package:open_file/open_file.dart';
@@ -9,37 +11,44 @@ import 'package:open_dir/open_dir.dart';
 
 import 'package:woniu/common/func.dart';
 
-List<Widget> diffGetButtons(List<Map<String,dynamic>> receviceFilesLog, int index, Function delAction){
+List<Widget> diffGetButtons(List<Map<String, dynamic>> receviceFilesLog,
+    int index, Function delAction) {
   //仅PC可以打开文件夹
-  if(Platform.isWindows || Platform.isLinux || Platform.isMacOS){
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
     return [
-          InkWell(
-            onTap: () async {
-              final rs = await OpenDir().openNativeDir(path: p.dirname(receviceFilesLog[index]["fileFullPath"]!));
-              //log(rs,StackTrace.current);
-            },
-            child: const Icon(Icons.drive_file_move_rounded),
-          ),
-          const SizedBox(width: 20),
-          InkWell(
-            onTap: () {
-              OpenFile.open(receviceFilesLog[index]["fileFullPath"]!).then(
-                (value){
-                  //log(value.message,StackTrace.current);
-                } 
-              ); 
-            },
-            child: const Icon(Icons.file_open),
-          ),
-          const SizedBox(width: 20),
-          InkWell(
-            onTap: () {
-              // ignore: unnecessary_this
-              delAction(receviceFilesLog[index]["fileFullPath"]);
-            },
-            child: const Icon(Icons.delete),
-          ),
-        ];
+      InkWell(
+        onTap: () async {
+          String path = Platform.isMacOS
+              ? receviceFilesLog[index]["fileFullPath"]
+              : p.dirname(receviceFilesLog[index]["fileFullPath"]!);
+          OpenDir().openNativeDir(path: path).then((value) {
+            if(value != true){
+              BotToast.showText(text: "无法打开该文件夹");
+            }
+          });
+        },
+        child: const Icon(Icons.drive_file_move_rounded),
+      ),
+      const SizedBox(width: 20),
+      InkWell(
+        onTap: () {
+          OpenFile.open(receviceFilesLog[index]["fileFullPath"]!).then((value) {
+            if (value.message.contains("errors")) {
+              BotToast.showText(text: "没有找到合适的应用程序打开该文件");
+            }
+          });
+        },
+        child: const Icon(Icons.file_open),
+      ),
+      const SizedBox(width: 20),
+      InkWell(
+        onTap: () {
+          // ignore: unnecessary_this
+          delAction(receviceFilesLog[index]["fileFullPath"]);
+        },
+        child: const Icon(Icons.delete),
+      ),
+    ];
   } else {
     return [
       // InkWell(
@@ -57,26 +66,25 @@ List<Widget> diffGetButtons(List<Map<String,dynamic>> receviceFilesLog, int inde
       const SizedBox(width: 20),
       InkWell(
         onTap: () {
-          Permission.manageExternalStorage.request().then((value){
-            if(value == PermissionStatus.granted){
+          Permission.manageExternalStorage.request().then((value) {
+            if (value == PermissionStatus.granted) {
               //检测是否为apk文件
-              if(p.extension(receviceFilesLog[index]["fileFullPath"]) == '.apk'){
-                log("这是apk",StackTrace.current);
-                Permission.requestInstallPackages.request().then((value){
-                  if(value == PermissionStatus.granted){
-                    OpenFile.open(receviceFilesLog[index]["fileFullPath"]!).then(
-                      (value){
-                        //log(value.message,StackTrace.current);
-                      } 
-                    ); 
+              if (p.extension(receviceFilesLog[index]["fileFullPath"]) ==
+                  '.apk') {
+                log("这是apk", StackTrace.current);
+                Permission.requestInstallPackages.request().then((value) {
+                  if (value == PermissionStatus.granted) {
+                    OpenFile.open(receviceFilesLog[index]["fileFullPath"]!)
+                        .then((value) {
+                      //log(value.message,StackTrace.current);
+                    });
                   }
                 });
               } else {
-                OpenFile.open(receviceFilesLog[index]["fileFullPath"]!).then(
-                  (value){
-                    //log(value.message,StackTrace.current);
-                  } 
-                ); 
+                OpenFile.open(receviceFilesLog[index]["fileFullPath"]!)
+                    .then((value) {
+                  //log(value.message,StackTrace.current);
+                });
               }
             }
           });
@@ -93,7 +101,4 @@ List<Widget> diffGetButtons(List<Map<String,dynamic>> receviceFilesLog, int inde
       ),
     ];
   }
-  
 }
-
-
