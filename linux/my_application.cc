f7a60318528a38bebdadf1f9bf7ec711d6d3a1be
digitalevers <opensource@digitalevers.com>
2024-01-24 14:22:39 +0800

    fix

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "woniu");
+    gtk_header_bar_set_title(header_bar, "tuotu");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "woniu");
+    gtk_window_set_title(window, "tuotu");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
